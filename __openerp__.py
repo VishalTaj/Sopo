46f485eb43d9008a1aa6aa6d88b98a0fd2ffba98
Vishal Taj PM <getvishalonline@gmail.com>
2015-04-27 17:28:08 +0530

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,10 @@
+{
+"name": "Sopo",
+"version": "1.1",
+"author": "Vishal Taj PM",
+"category": "Shipment Sales",
+"depends": ["sale", "purchase"],
+"data": ["sopo.xml"],
+"installable": True,
+"active": False,
+}
